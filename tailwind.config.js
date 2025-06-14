6296848d290a0a5e8d9bb4bb1a98372060277e15
NIDHI SONI <soninidhi630@gmail.com>
2025-06-14 16:36:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Enables manual dark mode toggle via 'dark' class
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
